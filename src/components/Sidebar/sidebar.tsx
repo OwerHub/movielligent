646893afc242eb4c5959b1fb18d51e0c939ffead
OwerWild <owerwild@gmail.com>
2022-09-03 02:58:29 +0200

    get movies from lS

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -1,41 +1,32 @@
-import { oneMovie } from "../../types/movietypes"
-import { useState , useEffect} from "react"
+import { oneMovie } from "../../types/movietypes";
+import { useState, useEffect } from "react";
 
-export  const Sidebar = () =>  {
-const [isMovieList, setMovieList] = useState<oneMovie[] | null>()
+export const Sidebar = () => {
+  const [isMovieList, setMovieList] = useState<oneMovie[] | null>();
 
-const getLocalStorage = () => {
-   
-    let movieList = window.localStorage.getItem("movielligent")
+  const getLocalStorage = () => {
+    let movieList = window.localStorage.getItem("movielligent");
 
-    if (movieList) {
-       movieList = JSON.parse(movieList) 
+    if (movieList !== null) {
+      setMovieList(JSON.parse(movieList) )
     }
-
-    console.log(movieList)
-} 
-
-
-useEffect(() => {
-    getLocalStorage()
-}, [])
-
-useEffect(() => {
-
-    console.log("localstorage Changed")
-
-}, [window.localStorage.getItem("movielligent") ] )
+    if (movieList === null) {
+        setMovieList(null)
+    }
+    
+  };
 
 
-    return(
-        <div>
-            this is sidebar
-            {isMovieList && isMovieList.map((movie, iterator) => (
-                <div>
-                    {movie.title}
-                </div>
-            ))}
-        </div>
-    )
 
-}
+  useEffect(() => {
+    getLocalStorage();
+  }, []);
+
+  return (
+    <div>
+      this is sidebar
+      {isMovieList &&
+        isMovieList.map((movie, iterator) => <div>{movie.title}</div>)}
+    </div>
+  );
+};
