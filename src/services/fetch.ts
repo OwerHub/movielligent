96f5657812926f14c71ed90609319387b016d545
OwerWild <owerwild@gmail.com>
2022-09-06 02:32:10 +0200

    Empty imput pagechange fixed

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -5,22 +5,38 @@ interface searchQueryParameters {
   actualPage: number;
 }
 
-// ezt majd ENV_ből
-const APIKey = "0e5e769f060ca0f15ec6dbad6a46c3b4";
-console.log("api key in Fetch", process.env.REACT_APP_API_KEY)
-console.log("api key2 in Fetch ", process.env.REACT_APP_API_KEY2)
 const pageMinimum = (pageNumber: number) :number=>{
   return pageNumber >= 1 ? pageNumber : 1
 }
+
+let previousKeyword = ""
+
 
 export async function getSearchedMovies<T>({
   searchText,
   actualPage,
 }: searchQueryParameters): Promise<T> {
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${APIKey}&language=en-US&query=${decodeURI(
+
+
+
+  // handle when the user delete the input field and changfe page
+  let searchWordInner : string = ""
+  if (searchText.length> 3) {
+    previousKeyword = searchText
+    searchWordInner = searchText
+  } 
+  if (searchText.length <=3 && previousKeyword.length > 3) {
+    searchWordInner = previousKeyword
+  }
+
+// itt kell megnézni, hogy volt-e már ilyen searchText és ActualPage?  ha volt, egyből töltse vissza
+
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${decodeURI(
     searchText
   )}&page=${pageMinimum(actualPage)}&include_adult=false`;
 
   const { data } = await axios.get(url);
+
+  // a datát searchText-el és az actualPage-al együtt csapja hozzá a cache-hez
   return data;
 }
